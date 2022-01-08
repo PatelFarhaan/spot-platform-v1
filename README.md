6976baabd9398d3054db3afac3ee4674cfe653cb
Farhaan <patel.farhaaan@gmail.com>
2022-01-08 01:57:40 -0500

    monitoring completed

--- README.md
+++ README.md
@@ -42,24 +42,43 @@ need of the traffic.
 
 
 # TODO:
-- [ ] Run terrafrom on EC2 through ASSUME ROLE
-- [ ] Download/Upload an SSH key for enduser if he does not have one
-- [ ] Open LB for all availability zones and public subnets
-- [ ] Dynamically change the gunicorn processes and threads as per instance cores in each deployment
-- [ ] Create and manage the ACM accordingly
-- [ ] Certifacte expire and renew alters
-- [ ] Increase and decrease volume size
-- [ ] Build an end user dashboard where they can import their keys and check their instance utilization
 - [ ] EBS Affinity per AZ
-- [ ] Support EFS attachment for data persistance and ingest code to attach the EFS to instance at launch time
-- [ ] Create a docker-compose file for all agents -> End user instances
-- [ ] Create a docker-compile file for automation of Jenkins and other stuff -> server side
-- [ ] To know when to use and replace t3 unlimited to standard and vice versa
+- [ ] Increase and decrease volume size
+- [ ] Certifacte expire and renew alters
 - [ ] Support multiple instances for SPOT
+- [ ] Create and manage the ACM accordingly
+- [ ] Run terrafrom on EC2 through ASSUME ROLE
+- [ ] Open LB for all availability zones and public subnets
+- [ ] Download/Upload an SSH key for enduser if he does not have one
+- [ ] Create a docker-compose file for all agents -> End user instances
+- [ ] To know when to use and replace t3 unlimited to standard and vice versa
+- [ ] Create a docker-compile file for automation of Jenkins and other stuff -> server side
+- [ ] Dynamically change the gunicorn processes and threads as per instance cores in each deployment
+- [ ] Build an end user dashboard where they can import their keys and check their instance utilization
+- [ ] Support EFS attachment for data persistance and ingest code to attach the EFS to instance at launch time
+- [ ] Replace unhealthy instances
+- [ ] Create Events Rules for autoscaling and spot in Cloudwatch
+- [ ] Create an activity page for the frontend where all events from cloudwatch will be displayed
+- [ ] Dashboard where it displays all ASG and all instances in each, buttons on instances to connect to it + health in green or red
+- [ ] Checks on ebs volume threshold reaches beyond a certain limit
+- [ ] Validate Subscription is from SNS -> Decode the Signature and compare it with the token
+- [ ] Add Resource limits to docker container in compose file
 
 
-# CURRENTLY WORKING ON
+# END USER DASHBOARD
+- [ ] SemaPhore Docs for Ansible Agents
+- [ ] Logstash and Kibana for APM -> End user dashboard
+- [ ] Integrate Nexus as a docker registry -> End user dashboard
+
+# CURRENTLY, WORKING ON
+How to store promwtheus data on disk
+prometheus alerts
+Add authentication for /metrics endpoint
+
+agent to update the prometheus file dynamically and deplot all this via terraform
+
 Migrate to Spot Fleet
+Install monitoring agents to analyze the memory
 Jenkins CI to create custom baked images using Packer
 
 
@@ -67,3 +86,19 @@ Jenkins CI to create custom baked images using Packer
 ```bash
 aws ec2 describe-subnets --filter Name=vpc-id,Values=vpc-8d6cc4f6 --query 'Subnets[?MapPublicIpOnLaunch==`false`].SubnetId'
 ```
+
+# LINKS
+1. Run Jenkins CI on SPOT Fleet: https://www.youtube.com/watch?v=8gGItacZjps
+2. traefik: https://doc.traefik.io/traefik/routing/services/ and https://www.indivar.com/blog/how-to-setup-traefik-portainer-on-ubuntu1804/ and https://hub.docker.com/_/traefik
+3. Loki https://grafana.com/grafana/dashboards/14055 and https://grafana.com/grafana/dashboards/12611
+4. Concul: https://www.consul.io/docs/connect/observability/ui-visualization
+5. Cloudwatch spot sns: https://ec2spotworkshops.com/running_spark_apps_with_emr_on_spot_instances/tracking_spot_interruptions.html
+6. All Grafana Dashboards: https://play.grafana.org/d/000000012/grafana-play-home?orgId=1
+7. https://pinpoint-apm.github.io/pinpoint/#
+
+
+# KEYPOINTS
+1. ASG and LB will only have public subnets else public ip will not be provided
+
+
+Grafana dashboards used:
---
+++ webhooks/update_prometheus_file.py
@@ -0,0 +1,272 @@
+# <==================================================================================================>
+#                                          IMPORTS
+# <==================================================================================================>
+import os
+import sys
+import json
+import yaml
+import boto3
+import requests
+from flask_crontab import Crontab
+from flask import Flask, request, jsonify
+from botocore.exceptions import ClientError
+
+
+# <==================================================================================================>
+#                                          FLASK CONFIG
+# <==================================================================================================>
+app = Flask(__name__)
+crontab = Crontab(app)
+
+
+# <==================================================================================================>
+#                                          AWS CLIENT
+# <==================================================================================================>
+class AWS(object):
+    def __init__(self):
+        self.ec2 = "ec2"
+        self.autoscale = "autoscaling"
+        self.aws_region = os.getenv("AWS_REGION")
+        self.aws_profile = os.getenv("AWS_PROFILE")
+        self.session = boto3.Session(profile_name=self.aws_profile)
+        self.aws_config = {
+            "region_name": self.aws_region
+        }
+
+    def get_autoscale_client(self):
+        try:
+            as_client = self.session.client(self.autoscale, **self.aws_config)
+            return as_client
+        except ClientError as e:
+            print(f"Code Exception is: {e.response['Error']['Code']}")
+            print(f"Error Message is: {e.response['Error']['Message']}")
+            print(f"Status Code is: {e.response['ResponseMetadata']['HTTPStatusCode']}")
+            sys.exit(1)
+
+    def get_ec2_client(self):
+        try:
+            ec2_client = self.session.client(self.ec2, **self.aws_config)
+            return ec2_client
+        except ClientError as e:
+            print(f"Code Exception is: {e.response['Error']['Code']}")
+            print(f"Error Message is: {e.response['Error']['Message']}")
+            print(f"Status Code is: {e.response['ResponseMetadata']['HTTPStatusCode']}")
+            sys.exit(1)
+
+
+# <==================================================================================================>
+#                                          PARSE DATA
+# <==================================================================================================>
+def is_instance_launching(data):
+    message = json.loads(data["Message"])
+    detail = message.get("detail")
+    description = detail.get("Description")
+    instance_id = detail.get("EC2InstanceId")
+    asg_name = detail.get("AutoScalingGroupName")
+
+    if description.startswith("Terminating EC2 instance"):
+        return False, instance_id, asg_name
+    elif description.startswith("Launching a new EC2 instance"):
+        return True, instance_id, asg_name
+
+
+# <==================================================================================================>
+#                                     GET INSTANCE PUBLIC IP ADDRESS
+# <==================================================================================================>
+def get_instance_public_ip(instance_id):
+    print("Describe instance: ", instance_id)
+    aws_obj = AWS()
+    ec2_client = aws_obj.get_ec2_client()
+    response = ec2_client.describe_instances(
+        InstanceIds=[instance_id]
+    )
+    ipv4_address = response["Reservations"][0]["Instances"][0]["PublicIpAddress"]
+    return ipv4_address
+
+
+# <==================================================================================================>
+#                                      GET INSTANCE ENVIRONMENT
+# <==================================================================================================>
+def get_environment_details(asg_name):
+    data = read_json(env_ins_mapping_fn)
+
+    if data.get(asg_name):
+        return data[asg_name][0], data[asg_name][1]
+
+    aws_obj = AWS()
+    env = app_name = None
+    as_client = aws_obj.get_autoscale_client()
+    response = as_client.describe_tags(
+        Filters=[
+            {
+                'Name': 'auto-scaling-group',
+                'Values': [asg_name]
+            },
+        ],
+    )
+    for tags in response["Tags"]:
+        if tags["Key"] == "Environment":
+            env = tags["Value"]
+        if tags["Key"] == "Application":
+            app_name = tags["Value"]
+
+    data[asg_name] = [env, app_name]
+    save_json(env_ins_mapping_fn, data)
+    return env, app_name
+
+
+# <==================================================================================================>
+#                                  UPDATE INSTANCE DETAILS IN PROMETHEUS
+# <==================================================================================================>
+def update_instance_details(data, is_launch, environment, ipv4_address, application_name):
+    application_found = False
+    for index1, sc in enumerate(data["scrape_configs"]):
+        if sc.get("job_name") == "node-exporter":
+            for index2, config in enumerate(sc["static_configs"]):
+                if config.get("labels", {}).get("environment") == environment and \
+                        config.get("labels", {}).get("application") == application_name:
+                    application_found = True
+                    if is_launch:
+                        data["scrape_configs"][index1]["static_configs"][index2]["targets"].append(ipv4_address)
+                    else:
+                        data["scrape_configs"][index1]["static_configs"][index2]["targets"].remove(ipv4_address)
+
+    if not application_found:
+        new_application_payload = {
+            'job_name': 'node-exporter',
+            'metrics_path': '/metrics',
+            'scheme': 'http',
+            'static_configs': [
+                {
+                    'targets': [ipv4_address],
+                    'labels': {
+                        'environment': environment,
+                        'application': application_name,
+                    }
+                }
+            ]
+        }
+        data["scrape_configs"].append(new_application_payload)
+    return data
+
+
+# <==================================================================================================>
+#                                    CHECK IF INSTANCE IS PRESENT IN JSON
+# <==================================================================================================>
+def check_instance_is_present(instance_id, launch):
+    data = read_json(instanceid_ip_fn)
+    if launch:
+        if instance_id in data:
+            return True
+    else:
+        if instance_id not in data:
+            return True
+    return False
+
+
+# <==================================================================================================>
+#                                    SAVE INSTANCE IP ADDRESS IN JSON
+# <==================================================================================================>
+def save_instance_ip_mapping(instance_id, save_object):
+    data = read_json(instanceid_ip_fn)
+    data[instance_id] = save_object
+    save_json(instanceid_ip_fn, data)
+
+
+# <==================================================================================================>
+#                                          UPDATE PROMETHEUS FILE
+# <==================================================================================================>
+def update_prometheus_file(data):
+    is_launch, instance_id, asg_name = is_instance_launching(data)
+
+    if check_instance_is_present(instance_id, is_launch):
+        return jsonify({}), 200
+
+    if is_launch:
+        environment, application_name = get_environment_details(asg_name)
+        ipv4_address = get_instance_public_ip(instance_id)
+        ipv4_address = f"{ipv4_address}:9100"
+        save_obj = {
+            "environment": environment,
+            "ipv4_address": ipv4_address,
+            "application_name": application_name
+        }
+        save_instance_ip_mapping(instance_id, save_obj)
+    else:
+        data = read_json(instanceid_ip_fn)
+        instance_details = data.pop(instance_id)
+        environment = instance_details["environment"]
+        ipv4_address = instance_details["ipv4_address"]
+        application_name = instance_details["application_name"]
+        save_json(instanceid_ip_fn, data)
+
+    with open(prometheus_file_name) as rf:
+        data = yaml.load(rf, Loader=yaml.FullLoader)
+        update_instance_details(data, is_launch, environment, ipv4_address, application_name)
+
+    with open(prometheus_file_name, "w") as wf:
+        yaml.dump(data, wf)
+
+
+# <==================================================================================================>
+#                                          SNS NOTIFICATION
+# <==================================================================================================>
+@app.route("/webhook/instance-modification", methods=["POST"])
+def sns_notification():
+    data = json.loads(request.get_data())
+    update_prometheus_file(data)
+    reload_prometheus()
+    return jsonify({}), 200
+
+
+# <==================================================================================================>
+#                                          ROUTE ENDPOINT
+# <==================================================================================================>
+def reload_prometheus():
+    requests.post('http://prometheus:9090/-/reload')
+
+
+# <==================================================================================================>
+#                                          ROUTE ENDPOINT
+# <==================================================================================================>
+@app.route("/webhook/server-health")
+def health_check():
+    return jsonify({"status": "OK"})
+
+
+# <==================================================================================================>
+#                                          READ JSON
+# <==================================================================================================>
+def read_json(filename):
+    with open(filename, 'r') as openfile:
+        data = json.load(openfile)
+    return data
+
+
+# <==================================================================================================>
+#                                          SAVE JSON
+# <==================================================================================================>
+def save_json(filename, data):
+    with open(filename, "w") as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+# # <==================================================================================================>
+# #                                          CHECK ALL SERVER MANUALLY
+# # <==================================================================================================>
+# @crontab.job(minute="30")
+# def my_scheduled_job():
+#     do_something()
+
+
+# <==================================================================================================>
+#                                         MAIN FUNCTION
+# <==================================================================================================>
+if __name__ == '__main__':
+    env_ins_mapping_fn = "/application/data/environment_instance_mapping.json"
+    instanceid_ip_fn = "/application/data/instanceid_ip_mapping.json"
+    prometheus_file_name = "/application/data/prometheus.yml"
+    app.run(host="0.0.0.0", port=5000)
+
+# Todo: set a flask cron job to check the data every 30 mins
+# Todo: have a alert on null data
