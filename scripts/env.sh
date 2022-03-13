1c734fb0e67a58715b9871dea17cc561881d309c
Farhaan <patel.farhaaan@gmail.com>
2022-03-12 20:36:10 -0500

    Updated S3 logic in Jenkinsfile and deleted unnecessary files

--- scripts/env.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-export od_asg_name=od-20220127003320235000000007
-export spot_asg_name=spot-20220127003316510200000006
--- scripts/set_env_var.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-
-current_path=`pwd` &&
-echo "#! /bin/bash" > env.sh
-cd "/Users/personal/Projects/spot_platform/tf_runner/redflag/dev/us-east-1/service-lookup/tf_script" &&
-terraform output -json | jq ".outputs.value" | jq -r "to_entries|map(\"export \(.key)=\(.value|tostring)\")|.[]" >> "${current_path}/env.sh"
-source "${current_path}/env.sh"
