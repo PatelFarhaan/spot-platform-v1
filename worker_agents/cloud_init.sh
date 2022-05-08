632fa977c91e7e3c8f2ebf80be35ec325621a6fa
Farhaan <patel.farhaaan@gmail.com>
2022-05-08 01:31:32 -0400

    updated worker agents docker-compose code

--- worker_agents/cloud_init.sh
+++ worker_agents/cloud_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e -x
 no_of_cores=`nproc --all`
 APP_REPLICAS=$(( ($no_of_cores * 2) + 1 ))
 export APP_REPLICAS=$APP_REPLICAS
@@ -7,6 +8,6 @@ export APP_REPLICAS=$APP_REPLICAS
 export CLIENT_APP_IMAGE='***REMOVED***.dkr.ecr.us-east-1.amazonaws.com/redflag-api-lookup:development-242'
 export CLIENT_APP_PORT=5000
 
-export ENV=staging
 export HOSTNAME=`hostname`
+export ENVIRONMENT=staging
 export APPLICATION=redflag-api-lookup
--- worker_agents/users_data.sh
+++ worker_agents/users_data.sh
@@ -9,5 +9,5 @@ source spotops_cloud_init.sh
 
 cd /var/opt/spotops/agents
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ***REMOVED***.dkr.ecr.us-east-1.amazonaws.com
-aws s3 cp s3://spot-platform/redflag-api-lookup-staging/app.env
-sudo docker-compose up -d --build --force-recreate --remove-orphans
+aws s3 cp s3://spot-platform/redflag-api-lookup-staging/app.env ./
+sudo -E docker-compose up -d --build --force-recreate --remove-orphans
